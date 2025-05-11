5a474c423636b7dbbcc21d379d236c41e90c564c
akashjare13 <akashjare13@gmail.com>
2025-05-11 14:13:36 +0530

    Task01

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,53 @@
+package Tasks;
+
+public class Task1 {
+    // Task #1
+    //
+    //Table of 5 you need to print,  (only use the printF)
+    //int num = 5;
+    //
+    //5×1=5
+    //5×2 = 10
+    //….
+    //5×10 =50
+
+    public static void main(String[] args) {
+/*
+        int num = 5;
+        System.out.println(num + " x 1 = " + num * 1);
+        System.out.println(num + " x 2 = " + num * 2);
+        System.out.println(num + " x 3 = " + num * 3);
+        System.out.println(num + " x 4 = " + num * 4);
+        System.out.println(num + " x 5 = " + num * 5);
+        System.out.println(num + " x 6 = " + num * 6);
+        System.out.println(num + " x 7 = " + num * 7);
+        System.out.println(num + " x 8 = " + num * 8);
+        System.out.println(num + " x 9 = " + num * 9);
+        System.out.println(num + " x 10 = " + num * 10);
+*/
+
+        int number = 5;
+        System.out.printf("%d x 1 = %d\n", number, number * 1);
+        System.out.printf("%d x 2 = %d\n", number, number * 2);
+        System.out.printf("%d x 3 = %d\n", number, number * 3);
+        System.out.printf("%d x 4 = %d\n", number, number * 4);
+        System.out.printf("%d x 5 = %d\n", number, number * 5);
+        System.out.printf("%d x 6 = %d\n", number, number * 6);
+        System.out.printf("%d x 7 = %d\n", number, number * 7);
+        System.out.printf("%d x 8 = %d\n", number, number * 8);
+        System.out.printf("%d x 9 = %d\n", number, number * 9);
+        System.out.printf("%d x 10 = %d\n", number, number * 10);
+
+/*
+        int no = 5;
+        int result;
+        for(int i=1;i<=10;i++){
+            result = no * i;
+            System.out.println( no +" x "+ i +" = "+ result);
+        }
+
+*/
+    }
+
+
+    }
