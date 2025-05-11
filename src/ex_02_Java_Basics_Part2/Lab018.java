fa089e2c62dfc8a888d4985425cf43d4a8f81156
akashjare13 <akashjare13@gmail.com>
2025-05-11 12:16:36 +0530

    exerices

---
+++ src/ex_02_Java_Basics_Part2/Lab018.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        int _ = 65;
+        int $ = 65;
+        int year = 2025;
+        int this_is_a_vari_long_name_in_the_class_hello_$_ = 89;
+        //System.out.println(_);
+        System.out.println($);
+        System.out.println(year);
+        System.out.println(this_is_a_vari_long_name_in_the_class_hello_$_);
+
+
+    }
+}
---
+++ src/ex_02_Java_Basics_Part2/Lab020_Interview_QnA.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab020_Interview_QnA {
+
+    public static void main(String[] args) {
+        // int enum = 10;
+        int Enum = 10;
+        System.out.println(Enum);
+        int a_b = 89;
+        System.out.println(a_b);
+//        int pramod dutta  = 89;
+        int pramod$dutta = 90;
+        System.out.println(pramod$dutta);
+        int Lab020_Interview_QnA2 = 123;
+
+
+    }
+}
